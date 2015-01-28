fdd2045d42650a8416675a02ff4166d8bc46c3af
Mahder Neway <mneway@mahder01.local>
2015-01-28 09:31:04 -0500

    I hope this time it should work

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,12 @@
+<?php
+  require_once '../classes/Calculator.php';
+
+  class CalculatorTest extends PHPUnit_Framework_TestCase{
+
+    public function testAdd(){
+      $a = new Calculator();
+      $this->assertEquals(8, $a->add(4,4));
+    }
+
+  }//end class
+?>
